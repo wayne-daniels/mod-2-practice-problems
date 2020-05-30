3dc9e1a2004265d90341983883a8296900f6451c
wayne-daniels <wa.daniels@hotmail.com>
2020-05-30 18:53:20 +0000

    Completed Object Manipulation 2 Exercise

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+function invert(source){
+  let new_obj = {};
+
+  for (let prop in source) {
+    if (source.hasOwnProperty(prop)) {
+      new_obj[source[prop]] = prop;
+    }
+  }
+
+  return new_obj;
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+function omit(source, keys){
+  let target = {};
+  for (let i in source) {
+    if (keys.indexOf(i) >= 0) continue;
+    if (!Object.prototype.hasOwnProperty.call(source, i)) continue;
+    target[i] = source[i];
+  }
+  return target;
+}
